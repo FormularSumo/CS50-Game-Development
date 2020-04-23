f87dd3370996eb1105f30f75d97757733323e620
FormularSumo <formularsumo@outlook.com>
2020-04-23 13:31:33 +0100

    Changed window size to 1080p

--- pong-0/main.lua
+++ pong-0/main.lua
@@ -20,8 +20,8 @@
     modern systems.
 ]]
 
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
+WINDOW_WIDTH = 1920
+WINDOW_HEIGHT = 1080
 
 --[[
     Runs when the game first starts up, only once; used to initialize the game.
