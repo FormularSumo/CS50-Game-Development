7fb0e0bd758de3c34c43f6e9af543d35872c5d40
FormularSumo <formularsumo@outlook.com>
2020-05-02 10:02:27 +0100

    Changed font size, text and commenting

--- Pong/main.lua
+++ Pong/main.lua
@@ -20,12 +20,15 @@
     modern systems.
 ]]
 
+font = love.graphics.newFont(50)
+love.graphics.setFont(font)
+
 WINDOW_WIDTH = 1920
 WINDOW_HEIGHT = 1080
 
---[[
-    Runs when the game first starts up, only once; used to initialize the game.
-]]
+
+    --Runs when the game first starts up, only once; used to initialize the game.
+
 function love.load()
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         resizable = true,
@@ -35,14 +38,14 @@ function love.load()
 end
 
 
---[[
-    Called after update by LÖVE2D, used to draw anything to the screen, updated or otherwise.
-]]
+
+    --Called after update by LÖVE2D, used to draw anything to the screen, updated or otherwise.
+
 function love.draw()
     love.graphics.printf(
-        'Hello Pong!',          -- text to render
+        'Pong!',          -- text to render
         0,                      -- starting X (0 since we're going to center it based on width)
         WINDOW_HEIGHT / 2 - 6,  -- starting Y (halfway down the screen)
-        WINDOW_WIDTH,           -- number of pixels to center within (the entire screen here)
+        WINDOW_WIDTH,           -- number of pixels to allign within
         'center')               -- alignment mode, can be 'center', 'left', or 'right'
 end
