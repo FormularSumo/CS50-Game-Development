2fc22657854923008e23839ab62af519a4352718
FormularSumo <formularsumo@outlook.com>
2020-05-04 23:14:40 +0100

    Only score when ball is completely off screen

--- Pong/main.lua
+++ Pong/main.lua
@@ -126,13 +126,13 @@ function love.update(dt)
         player1:update(dt)
         player2:update(dt)
     end
-    
-    if ball.x < 0 then
+
+    if ball.x + ball.radius / 2 < 0 then
         P2Score = P2Score + 1
         ball:reset()
     end
 
-    if ball.x > WINDOW_WIDTH then
+    if ball.x - ball.radius / 2 > WINDOW_WIDTH then
         P1Score = P1Score + 1
         ball:reset()
     end
