25b720cbae1d0d53c4ac9715f578550f6857ad8a
FormularSumo <formularsumo@outlook.com>
2020-06-17 12:16:57 +0100

    Flappy bird now renders at a virtual resolution using the push library. Updated push.lua from version 0.2 to 0.4 which stops getPixelScale causing crashes in modern versions of LOVE2D - getDpiScale is used instead.

--- Flappy Bird/main.lua
+++ Flappy Bird/main.lua
@@ -2,7 +2,7 @@
     GD50
     Flappy Bird Remake
 
-  Author: Formularsumo, based off Colton Ogden's version - cogden@cs50.harvard.edu
+    Author: Formularsumo, based off Colton Ogden's version - cogden@cs50.harvard.edu
 
     A mobile game by Dong Nguyen that went viral in 2013, utilizing a very simple 
     but effective gameplay mechanic of avoiding pipes indefinitely by just tapping 
@@ -13,12 +13,12 @@
     as an infinitely generated obstacle course for the player.
 ]]
 
+-- virtual resolution handling library
+push = require 'push'
 
--- physical screen dimensions
-WINDOW_WIDTH = 1920
-WINDOW_HEIGHT = 1080
-
-
+-- virtual resolution dimensions
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
 
 -- images we load into memory from files to later draw onto the screen
 local background = love.graphics.newImage('background.png')
@@ -29,12 +29,11 @@ function love.load()
     love.window.setTitle('Flappy Bird')
 
     -- initialize our virtual resolution
-    love.window.setMode(0,0, {
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, 0, 0, {
+        vsync = true,
         fullscreen = true,
-        resizable = true,
-        vsync = true
+        resizable = true
     })
-    WINDOW_WIDTH, WINDOW_HEIGHT = love.graphics.getDimensions()
 end
 
 
@@ -69,18 +68,20 @@ function love.keypressed(key)
     -- if key == 'f5' then 
     --     reset()
     -- end
-    --1/2 toggles player1/2.ai
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
 end
 
 function love.draw()
+    push:start()
+
     -- draw the background starting at top left (0, 0)
     love.graphics.draw(background, 0, 0)
 
     -- draw the ground on top of the background, toward the bottom of the screen
-    love.graphics.draw(ground, 0, WINDOW_HEIGHT - 16)
+    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
     
-end
-
-function love.update()
-    WINDOW_WIDTH, WINDOW_HEIGHT = love.graphics.getDimensions()
+    push:finish()
 end
